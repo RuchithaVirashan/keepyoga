6b04a8ca872e7b24947e19e2432d29758124502f
RuchithaVirashan <58288364+RuchithaVirashan@users.noreply.github.com>
2024-04-22 22:49:47 +0530

    add launch screen

--- lib/components/common/button.dart
+++ lib/components/common/button.dart
@@ -3,23 +3,19 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ButtonWidget extends StatelessWidget {
   final String buttonName;
-  final Color bcolor;
-  final Color fcolor;
-  final Color tcolor;
+  final Color buttonColor;
+  final Color textColor;
   final double radius;
   final double minHeight;
-  final Color borderColor;
   final void Function()? onPressed;
 
   const ButtonWidget({
     super.key,
     required this.buttonName,
-    required this.bcolor,
-    required this.tcolor,
+    required this.textColor,
     required this.radius,
-    required this.fcolor,
+    required this.buttonColor,
     required this.minHeight,
-    required this.borderColor,
     required this.onPressed,
   });
 
@@ -27,11 +23,9 @@ class ButtonWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextButton(
       style: ElevatedButton.styleFrom(
-        foregroundColor: fcolor,
-        backgroundColor: bcolor,
+        backgroundColor: buttonColor,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(radius)),
-          side: BorderSide(color: borderColor),
         ),
         minimumSize: Size(double.infinity, minHeight),
         padding: EdgeInsets.all(8.r),
@@ -40,7 +34,7 @@ class ButtonWidget extends StatelessWidget {
       child: Text(
         buttonName,
         style: TextStyle(
-          color: tcolor,
+          color: textColor,
           fontWeight: FontWeight.bold,
           fontSize: 16,
         ),
--- lib/screens/launch_screen.dart
+++ lib/screens/launch_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../components/common/button.dart';
+import '../components/common/default_text.dart';
 
 class LaunchScreen extends StatefulWidget {
   const LaunchScreen({super.key});
@@ -12,10 +16,88 @@ class _LaunchScreenState extends State<LaunchScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset("assets/Image.png"),
-          ],
+        child: Container(
+          color: const Color(0xFFDAE2EB),
+          height: ScreenUtil().screenHeight,
+          child: Stack(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 145.h),
+                child: Image.asset(
+                  "assets/Image.png",
+                  fit: BoxFit.cover,
+                  width: ScreenUtil().screenWidth,
+                  height: ScreenUtil().screenHeight,
+                ),
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 76.h),
+                    child: const Center(
+                      child: DefaultText(
+                        colorR: Color(0xFFAFBCCB),
+                        content: 'keepyoga',
+                        fontSizeR: 24,
+                        fontWeightR: FontWeight.w600,
+                        textAlignR: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 24.h,
+                      left: 24.w,
+                      right: 24.w,
+                    ),
+                    child: const Center(
+                      child: DefaultText(
+                        colorR: Color(0xFF090A0A),
+                        content: 'Practice yoga whenever you want.',
+                        fontSizeR: 32,
+                        fontWeightR: FontWeight.w700,
+                        textAlignR: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 490.h,
+                      left: 24.w,
+                      right: 24.w,
+                    ),
+                    child: Center(
+                      child: ButtonWidget(
+                        onPressed: () {},
+                        minHeight: 48.h,
+                        buttonName: 'Get started',
+                        textColor: const Color(0xFF090A0A),
+                        buttonColor: const Color(0xFFFFFFFF),
+                        radius: 48.r,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 10.h,
+                      left: 24.w,
+                      right: 24.w,
+                    ),
+                    child: Center(
+                      child: ButtonWidget(
+                        onPressed: () {},
+                        minHeight: 48.h,
+                        buttonName: 'Log in',
+                        textColor: const Color(0xFFFFFFFF),
+                        buttonColor: const Color(0xFF6B4EFF),
+                        radius: 48.r,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
